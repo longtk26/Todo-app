9431919939f339228f0408a07bbade280b953431
longtk26 <longtk26@gmail.com>
2023-02-26 23:33:36 +0700

    Add gitignore, procfile and update port

--- app.js
+++ app.js
@@ -130,6 +130,11 @@ app.post("/delete", (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server listening on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, () => {
+    console.log("Server has started successfully");
 });
